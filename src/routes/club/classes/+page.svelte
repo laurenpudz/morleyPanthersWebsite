9066417fcf6450f0d71d45cf5e1fbee745cca7de
laurenpudz <lhpudney@gmail.com>
2026-07-05 21:34:15 +0800

    eepy going to eeps

--- src/routes/club/classes/+page.svelte
+++ src/routes/club/classes/+page.svelte
@@ -1,7 +1,40 @@
 <script lang="ts">
-    import BasicPageWrapper from "../../../components/basicPageWrapper.svelte";
+	import BasicPageWrapper from '../../../components/basicPageWrapper.svelte';
 </script>
 
-<BasicPageWrapper title="Learn to Skate Classes">
-    <p>contained!!</p>
+<BasicPageWrapper title="Skate Classes">
+	<p>
+		The Learn to Skate Classes on Saturday and Sunday mornings are run by the Morley Rollerdrome,
+		but the Morley Panthers Rollerskating Club - or, more particularly, the artistic coaches and
+		skaters of the club - provide much of the teaching. We do this because we have a close
+		cooperative relationship with the Rollerdrome. The rink supports the club in many ways, and we
+		like to respond. We also find many of our competitive skaters from the ranks of those who pass
+		through the classes. But we know that most do not become club members, and that's fine. It's a
+		victory for all of us when a child (or an adult, for that matter) finds a recreation they enjoy,
+		whether they go on to become an Australian champion or simply keep coming to Rollerdrome, and
+		enjoying skating with their friends.
+	</p>
+	<p class="font-heading text-3xl pt-5">The Class Levels</p>
+	<p>
+		There are five levels in our classes, the first called "Entry Level", the rest numbered 1 to 4.
+	</p>
+
+	<div class="flex gap-5 flex-wrap pt-10">
+		<div class="bg-neutral-dark p-5">
+			<p class="font-heading text-2xl">Entry Level</p>
+			<p>
+				This is where new skaters learn to balance and roll on their skates, and gain some
+				confidence. <br/> 
+                The items to be mastered at this level are:
+			</p>
+            <ul>
+                <li>How to get down to and up from the floor</li>
+                <li>How to stand on skates - posture and balance</li>
+                <li>Balancing on one leg</li>
+                <li>Marching steps - begin moving</li>
+                <li>Stopping - with the brake rubber, on the front of roller skates or the back of inlines</li>
+                <li>Glides - basic, showing an ability to roll</li>
+            </ul>
+		</div>
+	</div>
 </BasicPageWrapper>
