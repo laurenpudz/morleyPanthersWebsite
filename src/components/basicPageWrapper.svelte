6a82a54c9eac75f32f7cd9ff0f2b5af000a5d70a
laurenpudz <lhpudney@gmail.com>
2026-07-06 11:02:09 +0800

    basic learn to sate page

--- src/components/basicPageWrapper.svelte
+++ src/components/basicPageWrapper.svelte
@@ -1,11 +1,10 @@
-<script>
+<script lang="ts">
 	import FeatureHeading from './featureHeading.svelte';
-
 	let { title, children } = $props();
 </script>
 
 <FeatureHeading {title} />
 
-<div class="px-5 xl:px-0 flex flex-col items-start">
+<div class="px-5 xl:px-0 pb-10 flex flex-col items-start">
 	{@render children?.()}
 </div>
